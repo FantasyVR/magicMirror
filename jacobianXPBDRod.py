5309e45bcfe9b0df9e6867968f199710d8f4e000
FantasyVR <y2505418927@gmail.com>
2021-06-25 13:00:33 +0800

    Correct Jacobian Solver

--- jacobianXPBDRod.py
+++ jacobianXPBDRod.py
@@ -2,18 +2,22 @@
 We use XPBD(gpu version) to simulate springs
 """
 import taichi as ti
+from taichi.lang.ops import abs, sqrt
 
 ti.init(arch=ti.gpu)
 h = 0.01  # timestep size
+squareInverseTime = 1.0/(h * h)
 compliance = 1.0e-6
-alpha = compliance * (1.0 / h / h)
-N = 100  # number of particles
+alpha = compliance * squareInverseTime
+N = 200 # number of particles
 NC = N - 1  # number of distance constraint
-NStep = 5
-NMaxIte = 100
+NStep = 2
+NMaxIte = 20
+omega = 1.0 # Relaxtion ratio
 pos = ti.Vector.field(2, float, N)
 accpos = ti.Vector.field(2, float, N)
 oldPos = ti.Vector.field(2, float, N)
+predictionPos = ti.Vector.field(2, float, N)
 vel = ti.Vector.field(2, float, N)
 disConsIdx = ti.Vector.field(2, int, NC)
 disConsLen = ti.field(float, NC)
@@ -21,6 +25,9 @@ invmass = ti.field(dtype=float, shape=N)
 lagrangian = ti.field(dtype=float, shape=NC)
 gravity = ti.Vector([0, -0.98])
 
+# For validation
+dualResidual = ti.field(float,())
+primalResidual = ti.field(float,())
 
 @ti.kernel
 def initRod():
@@ -30,7 +37,6 @@ def initRod():
         vel[i] = ti.Vector([0.0, 0.0])
         invmass[i] = 1.0
     invmass[0] = 0.0  # set the first particle static
-
 
 @ti.kernel
 def initConstraint():
@@ -46,27 +52,21 @@ def semiEuler():
             vel[i] += h * gravity
             oldPos[i] = pos[i]
             pos[i] += h * vel[i]
-@ti.kernel 
-def updatePV(): 
-    #update positions and velocities
-    for i in range(N):
-        if (invmass[i] != 0.0):
-            pos[i] += accpos[i]
-            vel[i] = (pos[i] - oldPos[i]) / h
+            predictionPos[i] = pos[i]
+
 @ti.kernel
 def resetLagrangin():
     # # reset lambda
     for i in range(NC):
         lagrangian[i] = 0.0
-@ti.func
+@ti.kernel
 def resetAccPos():
     for i in range(N):
         accpos[i] = ti.Vector([0.0,0.0])
 
 @ti.kernel
-def update():
+def update(ts:ti.i32, ite:ti.i32):
     # solve constriant
-    resetAccPos()
     for i in range(NC):
         idx1,idx2 = disConsIdx[i]
         rest_len = disConsLen[i]
@@ -81,13 +81,72 @@ def update():
         deltaLagrangian = -(constraint + lagrangian[i] * alpha) / (
             sumInvMass + alpha)
         lagrangian[i] += deltaLagrangian
+        # print("[",ts, ",", ite,"], dL", i , ":", deltaLagrangian,", lambda:", lagrangian[i])
         if invMass1 != 0.0:
-            accpos[idx1] += 0.9 * invMass1 * deltaLagrangian * gradient
+            accpos[idx1] +=  omega * invMass1 * deltaLagrangian * gradient
         if invMass2 != 0.0:
-            accpos[idx2] += -0.9 * invMass2 * deltaLagrangian * gradient
+            accpos[idx2] += -omega * invMass2 * deltaLagrangian * gradient
+
+@ti.kernel 
+def updateP(): 
+    #update positions and velocities
+    for i in range(N):
+        if (invmass[i] != 0.0):
+            pos[i] += accpos[i]
+            # print(i, "-th pos: ", pos[i])
+
+@ti.kernel
+def updateV():
+    for i in range(N):
+        if invmass[i] != 0.0:
+            vel[i] = (pos[i] - oldPos[i]) / h
+
+@ti.kernel
+def computeResidual():
+    dualResidual[None]  = 0.0
+    primalResidual[None] = 0.0
+    for i in range(NC):
+        idx1,idx2 = disConsIdx[i]
+        rest_len = disConsLen[i]
+        invMass1 = invmass[idx1]
+        invMass2 = invmass[idx2]
+        p1, p2 = pos[idx1], pos[idx2]
+        constraint = (p1 - p2).norm() - rest_len
+        
+        dualResidual[None] += abs(constraint - alpha * lagrangian[i])
+        
+        gradient = (p1 - p2).normalized()
+        r0 = ti.Vector([0.0,0.0])
+        r1 = r0
+        if invMass1 != 0.0:
+            r0 =   1.0 / invMass1 * (p1 -  predictionPos[idx1]) + lagrangian[i] * gradient
+            # r0 =  squareInverseTime *  1.0 / invMass1 * (p1 -  predictionPos[idx1]) - lagrangian[i] * gradient
+            # r0 = squareInverseTime * 1.0 / invMass1 * (p1 -  predictionPos[idx1]) + lagrangian[i] * gradient
+        if invMass2 != 0.0:
+            r1 =  1.0 / invMass2 * (p2 -  predictionPos[idx2]) - lagrangian[i] * gradient
+            # r1 =  squareInverseTime * 1.0 / invMass2 * (p2 -  predictionPos[idx2]) + lagrangian[i] * gradient
+            # r1 = squareInverseTime * 1.0 / invMass2 * (p2 -  predictionPos[idx2]) - lagrangian[i] * gradient
+            # print(p2,", ",predictionPos[idx2]," ", lagrangian[i],", ",gradient)
+        primalResidual[None] += sqrt(r0.norm_sqr() + r1.norm_sqr())
+
+    print("Dual Residual: ", dualResidual[None])
+    print("Primal Residual: ", primalResidual[None])
+
 
 initRod()
 initConstraint()
+# for i in range(NStep):
+#     semiEuler()
+#     resetLagrangin()
+#     #resetAccPos()
+#     for ite in range(NMaxIte):
+#         resetAccPos()
+#         update(i,ite)
+#         updateP()
+#     updateV()
+#     computeResidual()
+
+
 gui = ti.GUI('XPBD')
 pause = True
 while gui.running:
@@ -101,8 +160,11 @@ while gui.running:
             semiEuler()
             resetLagrangin()
             for ite in range(NMaxIte):
-                update()
-            updatePV()
+                resetAccPos()
+                update(i,ite)
+                updateP()
+            updateV()
+            computeResidual()
 
     position = pos.to_numpy()
     begin = position[:-1]
--- xpbd.py
+++ xpbd.py
@@ -2,6 +2,7 @@
 We use XPBD(cpu version) to simulate springs
 """
 import taichi as ti
+from taichi.lang.ops import abs, sqrt
 
 ti.init(arch=ti.cpu)
 h = 0.01  # timestep size
@@ -13,6 +14,7 @@ NStep = 5
 NMaxIte = 50
 pos = ti.Vector.field(2, float, N)
 oldPos = ti.Vector.field(2, float, N)
+predictionPos = ti.Vector.field(2, float, N)
 vel = ti.Vector.field(2, float, N)
 disConsIdx = ti.Vector.field(2, int, NC)
 disConsLen = ti.field(float, NC)
@@ -20,7 +22,9 @@ invmass = ti.field(dtype=float, shape=N)
 lagrangian = ti.field(dtype=float, shape=NC)
 gravity = ti.Vector([0, -0.98])
 
-
+# For validation
+dualResidual = ti.field(float,())
+primalResidual = ti.field(float,())
 @ti.kernel
 def initRod():
     for i in range(N):
@@ -30,61 +34,101 @@ def initRod():
         invmass[i] = 1.0
     invmass[0] = 0.0  # set the first particle static
 
-
 @ti.kernel
 def initConstraint():
     for i in range(NC):
         disConsIdx[i] = ti.Vector([i, i + 1])
         disConsLen[i] = (pos[i + 1] - pos[i]).norm()
 
-
-@ti.func
-def update():
+@ti.kernel
+def semiEuler():
     # semi-euler update pos & vel
     for i in range(N):
         if (invmass[i] != 0.0):
             vel[i] += h * gravity
             oldPos[i] = pos[i]
             pos[i] += h * vel[i]
+            predictionPos[i] = pos[i]
+
+@ti.kernel
+def resetLagrangian():
     # # reset lambda
     for i in range(NC):
         lagrangian[i] = 0.0
+
+@ti.kernel
+def update(ts:ti.i32, ite:ti.i32):
     # # solve constriant
-    for ite in range(NMaxIte):
-        for i in range(NC):
-            x,y = disConsIdx[i]
-            index = disConsIdx[i]
-            rest_len = disConsLen[i]
-            idx1 = index[0]
-            idx2 = index[1]
-            invMass1 = invmass[idx1]
-            invMass2 = invmass[idx2]
-            sumInvMass = invMass1 + invMass2
-            if sumInvMass < 1.0e-6:
-                print("Wrong Mass Setting")
-            p1 = pos[idx1]
-            p2 = pos[idx2]
-            constraint = (p1 - p2).norm() - rest_len
-            gradient = (p1 - p2).normalized()
-            deltaLagrangian = -(constraint + lagrangian[i] * alpha) / (
-                sumInvMass + alpha)
-            lagrangian[i] += deltaLagrangian
-            if invMass1 != 0.0:
-                pos[idx1] += invMass1 * deltaLagrangian * gradient
-            if invMass2 != 0.0:
-                pos[idx2] += -invMass2 * deltaLagrangian * gradient
+    for i in range(NC):
+        idx1,idx2 = disConsIdx[i]
+        rest_len = disConsLen[i]
+        invMass1 = invmass[idx1]
+        invMass2 = invmass[idx2]
+        sumInvMass = invMass1 + invMass2
+        if sumInvMass < 1.0e-6:
+            print("Wrong Mass Setting")
+        p1, p2 = pos[idx1], pos[idx2]
+        constraint = (p1 - p2).norm() - rest_len
+        gradient = (p1 - p2).normalized()
+        deltaLagrangian = -(constraint + lagrangian[i] * alpha) / (
+            sumInvMass + alpha)
+        lagrangian[i] += deltaLagrangian
+        # print("[",ts, ",", ite,"], dL", i , ":", deltaLagrangian,", lambda:", lagrangian[i])
+        if invMass1 != 0.0:
+            pos[idx1] += invMass1 * deltaLagrangian * gradient
+            # print(idx1, "-th pos: ", pos[idx1])
+
+        if invMass2 != 0.0:
+            pos[idx2] += -invMass2 * deltaLagrangian * gradient
+            # print(idx2, "-th pos: ", pos[idx2])
+
+
+@ti.kernel
+def updatePV():
     # # update pos and vel
     for i in range(N):
         if (invmass[i] != 0.0):
             vel[i] = (pos[i] - oldPos[i]) / h
 
 @ti.kernel
-def timestep():
-    for i in range(NStep):
-        update()
+def computeResidual():
+    dualResidual[None]  = 0.0
+    primalResidual[None] = 0.0
+    for i in range(NC):
+        idx1,idx2 = disConsIdx[i]
+        rest_len = disConsLen[i]
+        invMass1 = invmass[idx1]
+        invMass2 = invmass[idx2]
+        p1, p2 = pos[idx1], pos[idx2]
+        constraint = (p1 - p2).norm() - rest_len
+        
+        dualResidual[None] += abs(constraint - alpha * lagrangian[i])
+        
+        gradient = (p1 - p2).normalized()
+        r0 = ti.Vector([0.0,0.0])
+        r1 = r0
+        if invMass1 != 0.0:
+            r0 =   1.0 / invMass1 * (p1 -  predictionPos[idx1]) + lagrangian[i] * gradient
+        if invMass2 != 0.0:
+            r1 =  1.0 / invMass2 * (p2 -  predictionPos[idx2]) - lagrangian[i] * gradient
+            # print(p2,", ",predictionPos[idx2],", ", lagrangian[i],", ",gradient)
+        primalResidual[None] += sqrt(r0.norm_sqr() + r1.norm_sqr())
+    print("Dual Residual: ", dualResidual[None])
+    print("Primal Residual: ", primalResidual[None])
+
 
 initRod()
 initConstraint()
+
+# for i in range(NStep):
+#     semiEuler()
+#     resetLagrangian()
+#     for ite in range(NMaxIte):
+#         update(i, ite)
+#     updatePV()
+#     computeResidual()
+
+
 gui = ti.GUI('XPBD')
 pause = True
 while gui.running:
@@ -94,7 +138,13 @@ while gui.running:
         if e.key == gui.SPACE:
             pause = not pause
     if not pause:
-        timestep()
+        for i in range(NStep):
+            semiEuler()
+            resetLagrangian()
+            for ite in range(NMaxIte):
+                update()
+            updatePV()
+            computeResidual()
 
     position = pos.to_numpy()
     begin = position[:-1]
