77bc4afa734d649eb7d65d9484228871bdaed4c3
FantasyVR <6712304+FantasyVR@users.noreply.github.com>
2021-11-22 18:37:08 +0800

    Update testCG.py

--- gs/comparison/testCG.py
+++ gs/comparison/testCG.py
@@ -4,21 +4,23 @@ def CG(A,b, x0):
     assert(m == n and m == b.shape[0] and b.shape == x0.shape)
     residual = 1.0e-6
     r0 = b - A @ x0
+    print(f"r0: {r0}")
+
     if np.linalg.norm(r0) < residual:
         return x0
     x = x0
     p = r0
     r = r0
     count = 0
-    while True:
+    epsNew = sum(r * r)
+    while np.linalg.norm(r) > residual:
         Ap = A @ p 
-        rr = sum(r * r)
-        alpha = rr / sum(p * Ap)
+        alpha = epsNew / sum(p * Ap)
         x = x + alpha * p
         r_next = r - alpha * Ap
-        if np.linalg.norm(r_next) < residual:
-            break
-        beta = sum(r_next * r_next)/ rr 
+        epsOld = epsNew
+        epsNew = sum(r_next * r_next)
+        beta = epsNew / epsOld 
         p = r_next + beta * p
         r = r_next
         count += 1
@@ -29,6 +31,6 @@ b = np.array([5,6]).reshape(2,1)
 x0 = np.zeros((2,1))
 
 x = np.linalg.inv(A) @ b
-print(f"Real solution: {x}")
+print(f"Real solution:\n {x}")
 tx, numIter  = CG(A,b,x0)
-print(f"T solution: {tx}")
+print(f"T solution: \n {tx}")
